2b9523363c99515248943094ad3a436465405632
Nitesh Mali <niteshmali@nitesh-mali.local>
2024-04-17 14:24:48 +0530

    user create with shell

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
-from .models import Emp,Blogpost
+
+from .models import Cookie, Customers, Emp,Blogpost, Faculty, Section
 # Register your models here.
 admin.site.register(Emp)
 admin.site.register(Blogpost)
+admin.site.register(Faculty)
+admin.site.register(Customers)
+admin.site.register(Section)
+admin.site.register(Cookie)
+
 
 
--- myapp/models.py
+++ myapp/models.py
@@ -55,6 +55,21 @@ class Cookie(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
 
+# from django.db import models
+
+# class Category(models.Model):
+#     name = models.CharField(max_length=100)
+#     description = models.TextField()
+
+# class Book(models.Model):
+#     title = models.CharField(max_length=200)
+#     author = models.CharField(max_length=100)
+#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+#     description = models.TextField()
+#     price = models.DecimalField(max_digits=10, decimal_places=2)
+#     stock = models.PositiveIntegerField()
+#     image_url = models.URLField()
+#     video_url = models.URLField(blank=True, null=True)  # Optional video URL
 
      
 
